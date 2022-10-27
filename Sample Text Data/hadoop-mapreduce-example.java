bf483967afcf1e643d5877319b6e906390916a21
Mohd Hafizul Afifi Abdullah <16542981+hafizulamz@users.noreply.github.com>
2022-10-28 02:26:36 +0800

    Update hadoop-mapreduce-example.java

--- Sample Text Data/hadoop-mapreduce-example.java
+++ Sample Text Data/hadoop-mapreduce-example.java
@@ -1,42 +1,62 @@
-public static class Map extends Mapper < LongWritable, Text, Text, IntWritable > {
-  public void map(LongWritable key, Text value, Context context) throws IOException,
-  InterruptedException {
-    String line = value.toString();
-    StringTokenizer tokenizer = new StringTokenizer(line);
-    while (tokenizer.hasMoreTokens()) {
-      value.set(tokenizer.nextToken());
-      context.write(value, new IntWritable(1));
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+  public static class TokenizerMapper
+  extends Mapper < Object, Text, Text, IntWritable > {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException,
+    InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
     }
   }
-}
 
-public static class Reduce extends Reducer < Text, IntWritable, Text, IntWritable > {
-  public void reduce(Text key, Iterable < IntWritable > values, Context context) throws IOException,
-  InterruptedException {
-    int sum = 0;
-    for (IntWritable x: values) {
-      sum += x.get();
+  public static class IntSumReducer
+  extends Reducer < Text, IntWritable, Text, IntWritable > {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable < IntWritable > values,
+      Context context
+    ) throws IOException,
+    InterruptedException {
+      int sum = 0;
+      for (IntWritable val: values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
     }
-    context.write(key, new IntWritable(sum));
   }
-}
 
-public static void main(String[] args) throws Exception {
-  Configuration conf = new Configuration();
-  Job job = new Job(conf, "My Word Count Program");
-  job.setJarByClass(WordCount.class);
-  job.setMapperClass(Map.class);
-  job.setReducerClass(Reduce.class);
-  job.setOutputKeyClass(Text.class);
-  job.setOutputValueClass(IntWritable.class);
-  job.setInputFormatClass(TextInputFormat.class);
-  job.setOutputFormatClass(TextOutputFormat.class);
-  Path outputPath = new Path(args[1]);
-  //Configuring the input/output path from the filesystem into the job
-  FileInputFormat.addInputPath(job, new Path(args[0]));
-  FileOutputFormat.setOutputPath(job, new Path(args[1]));
-  //deleting the output path automatically from hdfs so that we don't have to delete it explicitly
-  outputPath.getFileSystem(conf).delete(outputPath);
-  //exiting the job only if the flag value becomes false
-  System.exit(job.waitForCompletion(true) ? 0 : 1);
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
